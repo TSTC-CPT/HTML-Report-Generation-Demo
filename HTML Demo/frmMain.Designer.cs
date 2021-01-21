873e1a4e104dda09b25adb4a6301695ac49db7c9
Dave <david.kuehne@tstc.edu>
2021-01-21 11:15:13 -0600

    Add project files.

---
+++ HTML Demo/frmMain.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace HTML_Demo
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFirst = new System.Windows.Forms.Label();
+            this.tbxFirstName = new System.Windows.Forms.TextBox();
+            this.tbxLastName = new System.Windows.Forms.TextBox();
+            this.lblLast = new System.Windows.Forms.Label();
+            this.tbxAge = new System.Windows.Forms.TextBox();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lbxPeople = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.tbxTitle = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblFirst
+            // 
+            this.lblFirst.AutoSize = true;
+            this.lblFirst.Location = new System.Drawing.Point(25, 65);
+            this.lblFirst.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblFirst.Name = "lblFirst";
+            this.lblFirst.Size = new System.Drawing.Size(106, 24);
+            this.lblFirst.TabIndex = 0;
+            this.lblFirst.Text = "First Name:";
+            // 
+            // tbxFirstName
+            // 
+            this.tbxFirstName.Location = new System.Drawing.Point(135, 62);
+            this.tbxFirstName.Name = "tbxFirstName";
+            this.tbxFirstName.Size = new System.Drawing.Size(144, 29);
+            this.tbxFirstName.TabIndex = 1;
+            // 
+            // tbxLastName
+            // 
+            this.tbxLastName.Location = new System.Drawing.Point(135, 113);
+            this.tbxLastName.Name = "tbxLastName";
+            this.tbxLastName.Size = new System.Drawing.Size(144, 29);
+            this.tbxLastName.TabIndex = 2;
+            // 
+            // lblLast
+            // 
+            this.lblLast.AutoSize = true;
+            this.lblLast.Location = new System.Drawing.Point(25, 116);
+            this.lblLast.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblLast.Name = "lblLast";
+            this.lblLast.Size = new System.Drawing.Size(104, 24);
+            this.lblLast.TabIndex = 2;
+            this.lblLast.Text = "Last Name:";
+            // 
+            // tbxAge
+            // 
+            this.tbxAge.Location = new System.Drawing.Point(135, 163);
+            this.tbxAge.Name = "tbxAge";
+            this.tbxAge.Size = new System.Drawing.Size(144, 29);
+            this.tbxAge.TabIndex = 3;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(79, 166);
+            this.lblAge.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(50, 24);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "Age:";
+            // 
+            // lbxPeople
+            // 
+            this.lbxPeople.FormattingEnabled = true;
+            this.lbxPeople.ItemHeight = 24;
+            this.lbxPeople.Location = new System.Drawing.Point(285, 62);
+            this.lbxPeople.Name = "lbxPeople";
+            this.lbxPeople.Size = new System.Drawing.Size(340, 124);
+            this.lbxPeople.TabIndex = 6;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(125, 198);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(154, 39);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "&Add Person";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(471, 198);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(154, 39);
+            this.btnCreate.TabIndex = 5;
+            this.btnCreate.Text = "&Create Report";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // tbxTitle
+            // 
+            this.tbxTitle.Location = new System.Drawing.Point(135, 14);
+            this.tbxTitle.Name = "tbxTitle";
+            this.tbxTitle.Size = new System.Drawing.Size(490, 29);
+            this.tbxTitle.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(15, 17);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(111, 24);
+            this.lblTitle.TabIndex = 9;
+            this.lblTitle.Text = "Report Title:";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(299, 198);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(154, 39);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "&Clear List";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(637, 258);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.tbxTitle);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lbxPeople);
+            this.Controls.Add(this.tbxAge);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.tbxLastName);
+            this.Controls.Add(this.lblLast);
+            this.Controls.Add(this.tbxFirstName);
+            this.Controls.Add(this.lblFirst);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HTML Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFirst;
+        private System.Windows.Forms.TextBox tbxFirstName;
+        private System.Windows.Forms.TextBox tbxLastName;
+        private System.Windows.Forms.Label lblLast;
+        private System.Windows.Forms.TextBox tbxAge;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.ListBox lbxPeople;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.TextBox tbxTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ HTML Demo/frmMain.cs
@@ -0,0 +1,204 @@
+﻿///<summary>
+///The purpose of this code is to demonstrate creating an HTML file that can be used to display reports,
+///receipts, and other types of similar output in a highly portable light weight manner. 
+///
+/// This is not an exhaustive demonstration of HTML or CSS nor is it a complete solution for any project.
+/// ---David Kuehne, CPT Instructor, January 2021
+///</summary>
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HTML_Demo
+{
+    public partial class frmMain : Form
+    {
+        private List<Person> people = new List<Person>(); // Global list of Person objects
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        //Person class that has the first name, last name, and age properties.
+        public class Person
+        {
+            public string strFirstName { get; set; }
+            public string strLastName { get; set; }
+            public int intAge { get; set; }
+
+            public Person(string strFirst, string strLast, int intPersonAge)
+            {
+                strFirstName = strFirst;
+                strLastName = strLast;
+                intAge = intPersonAge;
+            }
+        }
+
+        /// <summary>
+        /// Using StringBuilder to keep up with all the lines of text produced.
+        /// https://www.tutorialsteacher.com/csharp/csharp-stringbuilder
+        /// </summary>
+        /// <param name="title">This is used for the browser tab and the H1 tag.</param>
+        /// <returns>Will return all necessary code to produce a local HTML file.</returns>
+        private StringBuilder GenerateReport(string title)
+        {
+            StringBuilder html = new StringBuilder();
+            StringBuilder css = new StringBuilder();
+
+            // CSS is a way to style the HTML page. Each HTML tag can be customized.
+            // In this example, the H1 and TD tags are customized.
+            // Refer to this website for examples: https://www.w3schools.com/Css/css_syntax.asp
+
+            css.Append("<style>");
+            css.Append("td {padding: 5px; text-align:center; font-weight: bold; text-align: center;}");
+            css.Append("h1 {color: blue;}");
+            css.Append("</style>");
+
+            // HTML is used to format the layout of a webpage. This will be the frame
+            // we use to place our data in. CSS is used to style the page to look a
+            // certain way.
+
+            // The <HTML> and </HTML> tags are the start and end of a webpage.
+            // The <HEAD> and </HEAD> tags gives information about the webpage
+            // such as the title and if there is any CSS styles being used.
+            // The text between the <TITLE> and </TITLE> tags are used by the
+            // browser to display the name of the page.
+            // <BODY> and </BODY> is where the data of the page is stored
+            // <H1> and </H1> is the largest font size for headings. These
+            // can be from H1 to H6. H6 is the smallest font. https://www.w3schools.com/tags/tag_hn.asp
+
+            html.Append("<html>");
+            html.Append($"<head>{css}<title>{title}</title></head>");
+            html.Append("<body>");
+
+            html.Append($"<h1>{title}</h1>");
+
+            // Create table of data
+            // <TABLE> and </TABLE> is the start and end of a table of rows and data.
+            // <TR> and </TR> is one row of data. They contain <TD> and </TD> tags.
+            // <TD> and </TD> represents the data inside of the table in a particular row.
+            // https://www.w3schools.com/tags/tag_table.asp
+
+            // I used an <HR /> tag which is a "horizontal rule" as table data.
+            // You can "span" it across multiple columns of data.
+
+            html.Append("<table>");
+            html.Append("<tr><td>First Name</td><td>Last Name</td><td>Age</td></tr>");
+            html.Append("<tr><td colspan=3><hr /></td></tr>");
+            foreach (Person person in people)
+            {
+                html.Append("<tr>");
+                html.Append($"<td>{person.strFirstName}</td>");
+                html.Append($"<td>{person.strLastName}</td>");
+                html.Append($"<td>{person.intAge}</td>");
+                html.Append("</tr>");
+                html.Append("<tr><td colspan=4><hr /></td></tr>");
+            }
+            html.Append("</table>");
+            html.Append("</body></html>");
+
+            return html; // The returned value has all the HTML and CSS code to represent a webpage
+        }
+
+        /// <summary>
+        /// I separated the code that produced the HTML and CSS from the action
+        /// of creating the file on the hard drive then opening it. This could
+        /// be included in one function, but went with the separation of duties.
+        /// </summary>
+        /// <param name="html">This method expects all the code from the
+        /// StringBuilder we used earlier.</param>
+        private void PrintReport(StringBuilder html)
+        {
+            // Write (and overwrite) to the hard drive using the same filename of "Report.html"
+            try
+            {
+                // A "using" statement will automatically close a file after opening it.
+                // It never hurts to include a file.Close() once you are done with a file.
+                using (StreamWriter writer = new StreamWriter("Report.html"))
+                {
+                    writer.WriteLine(html);
+                }
+                System.Diagnostics.Process.Start(@"Report.html"); //Open the report in the default web browser
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("You currently do not have write permissions for this feature.",
+                    "Error with System Permissions", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            // If you want a unique filename you could use a date and time with part of a name
+            DateTime today = DateTime.Now;
+            using (StreamWriter writer = new StreamWriter($"{today.ToString("yyyy-MM-dd-HHmmss")} - Report.html"))
+            {
+                writer.WriteLine(html);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Validate that data was entered
+            // Prompt user of the error and give
+            // focus to the textbox with the error
+            if (tbxTitle.Text == String.Empty)
+            {
+                MessageBox.Show("Please enter a title for the report.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbxTitle.Focus();
+                return;
+            }
+            if (tbxFirstName.Text == String.Empty)
+            {
+                MessageBox.Show("Please enter a first name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbxFirstName.Focus();
+                return;
+            }
+            if (tbxLastName.Text == String.Empty)
+            {
+                MessageBox.Show("Please enter a last name.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbxLastName.Focus();
+                return;
+            }
+            if (!int.TryParse(tbxAge.Text, out _)) // The underscore represents discarding the data
+            {
+                MessageBox.Show("Age must be a whole number.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbxAge.Focus();
+                tbxAge.SelectAll();
+                return;
+            }
+
+            // Add the TextBox data into a new instance of Person and add the object to the list
+            people.Add(new Person(tbxFirstName.Text, tbxLastName.Text, int.Parse(tbxAge.Text)));
+            lbxPeople.Items.Add($"{tbxFirstName.Text} {tbxLastName.Text} - {tbxAge.Text} years old");
+
+            // Clear all TextBoxes except the title
+            tbxFirstName.Clear();
+            tbxLastName.Clear();
+            tbxAge.Clear();
+            tbxFirstName.Focus();
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (lbxPeople.Items.Count == 0)
+                MessageBox.Show("Nothing to report.", "No Data", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            else
+                PrintReport(GenerateReport(tbxTitle.Text));
+        }
+
+        //Reset TextBox controls, ListBox control, and List container
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lbxPeople.Items.Clear();
+            people.Clear();
+            tbxTitle.Clear();
+            tbxFirstName.Clear();
+            tbxLastName.Clear();
+            tbxAge.Clear();
+            tbxFirstName.Focus();
+            tbxTitle.Focus();
+        }
+    }
+}
